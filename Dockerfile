a74bcb730860e1b055a3335077f94b59eb259cd8
Levi Nelson <157050336+Nelson-Levi@users.noreply.github.com>
2026-03-03 12:57:41 -0700

    Add hello.yml

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM node:18-alpine
 
 WORKDIR /app
 
-COPY package.json .
+COPY package*.json .
 RUN npm install
 
 COPY . .
 
-EXPOSE 3000
+EXPOSE 8080
 
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const messages = [
   { id: 1, text: "Welcome to the Bulletin Board!" },
